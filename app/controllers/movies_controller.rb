4f634576373b6acaff1b741f0a244cf4b807e0d1
bigbrother13 <gritskevich.vladimir@gmail.com>
2019-04-23 18:29:08 +0300

    Add Test for search

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,7 @@
 class MoviesController < ApplicationController
   def index
     @movies = if params[:term]
+
       Movie.where('title LIKE ?', "%#{params[:term]}%")
     else
       Movie.all
--- spec/features/movies_spec.rb
+++ spec/features/movies_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Movie', type: :feature do
   let!(:movie)  { create :movie, content: 'asd' }
-  let!(:movie2) { create :movie, con }
+  let!(:movie2) { create :movie }
   let!(:movie3) { create :movie }
 
   describe 'visitor' do
@@ -15,15 +15,14 @@ RSpec.describe 'Movie', type: :feature do
     end
 
     it 'can search by content' do
-      fill_in 'Content', with: 'asd'
+      fill_in name="term", with: 'asd'
       click_button 'Search'
-      expect(page).to have_content('Movies')
+      expect(page).to have_content('ads')
     end
 
-    it 'can search by content' do
-      fill_in 'Content', with: 'asd'
-      click_button 'Search'
-      expect(page).to have_content('Movies')
-    end
+    # it 'go to new page' do
+    #   click_button '2'
+    #   expect(page).not_to have_link('link text', href: '...')
+    # ends
   end
 end
