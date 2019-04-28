f4c286a4956d0e9ec6ba28e6d31f00a511216ac0
bigbrother13 <gritskevich.vladimir@gmail.com>
2019-04-29 02:24:07 +0300

    Add scoper for model

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,22 +1,10 @@
 class MoviesController < ApplicationController
-
   def index
     @movies = Movie.all
-    if search_params[:title].present?
-      @movies = @movies.where('title LIKE ?', "%#{search_params[:title]}%")
-    end
-
-    if search_params[:content].present?
-      @movies = @movies.where('content LIKE ?', "%#{search_params[:content]}%")
-    end
-
-    if search_params[:quality] == '1'
-      @movies = @movies.where(quality: true)
-    end
-
-    if search_params[:voice] == '1'
-      @movies = @movies.where(voice: true)
-    end
+    @movies = @movies.with_title(search_params[:title]) if search_params[:title].present?
+    @movies = @movies.with_content(search_params[:content]) if search_params[:content].present?
+    @movies = @movies.with_quality if search_params[:quality] == '1'
+    @movies = @movies.with_voice if search_params[:voice] == '1'
   end
 
   private
@@ -25,3 +13,4 @@ class MoviesController < ApplicationController
     params[:search] || {}
   end
 end
+
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,6 @@
 class Movie < ApplicationRecord
+  scope :with_title, -> (title) { where('title LIKE ?', "%#{title}%")}
+  scope :with_content, -> (content) { where('content LIKE ?', "%#{content}%")}
+  scope :with_quality, -> { where(quality: true) }
+  scope :with_voice, -> { where(voice: true) }
 end
